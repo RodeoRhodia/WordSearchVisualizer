b8a2252140f5367c271c56c40c9e45bdb219f7b7
Lanz <lanzdagamer@gmail.com>
2022-08-04 13:44:12 -0400

    removed redundant sleep function

--- basicLogicWordSearch/wordSearchAsyncDemo1.js
+++ basicLogicWordSearch/wordSearchAsyncDemo1.js
@@ -1,19 +1,28 @@
 var BOARD = [['T', 'S'], ['A', 'B']];
+var WORD = "BATS";
 var ROWS = BOARD.length;
 var COLS = BOARD[0].length;
+var SECONDS = 1;
 
-// let taskX = (r) => {
-//     return new Promise((resolve) => {
-//       setTimeout(() => {
-        
-//       }, timeout);  
-//     })
-// }
-
-let taskY = (c) => {
-
+let sleep = () => {
+    return new Promise((resolve) => {
+      setTimeout(resolve, SECONDS * 1000);  
+    });
 }
 
 let processBoard = async () => {
-    
+    for (let r = 0; r < ROWS; r++) {
+        for (let c = 0; c < COLS; c++) {
+            await sleep();
+            console.log(BOARD[r][c]);
+
+            if (BOARD[r][c] == WORD[0]) {
+                await sleep();
+                console.log(`is ${BOARD[r][c]} equal to "B"? ${BOARD[r][c] == WORD[0]}`);
+            }
+
+        }
+    }
 }
+
+processBoard();
